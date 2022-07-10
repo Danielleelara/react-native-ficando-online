6785c58f5b2ccfb666900396c64fda0c15587664
danielleelara <dany.sa1@hotmail.com>
2022-07-10 18:18:54 -0300

    feat: criando metodos delete e put

--- src/paginas/Repositorios/index.js
+++ src/paginas/Repositorios/index.js
@@ -1,52 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, FlatList, TouchableOpacity } from 'react-native';
-import estilos from './estilos';
-import { pegarRepositorios } from '../../servicos/requisicoes/repositorios';
+import React, { useEffect, useState } from "react";
+import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import estilos from "./estilos";
+import { pegarRepositorios } from "../../servicos/requisicoes/repositorios";
+import { useIsFocused } from "@react-navigation/native";
 
 export default function Repositorios({ route, navigation }) {
-    const [repo, setRepo] = useState([]);
+  const [repo, setRepo] = useState([]);
+  const estaNaTela = useIsFocused();
 
-    useEffect(()=>{
-        async function busca(){
-            const resultado = await pegarRepositorios(route.params.id)
-            console.log(resultado)
-            if(resultado) {
-                setRepo(resultado)
-            }else{
-                return Alert.alert("Nenhum repositório encontrado!")
-                setRepo([])
-            }
+  useEffect(() => {
+    async function busca() {
+      const resultado = await pegarRepositorios(route.params.id);
+      if (resultado) {
+        setRepo(resultado);
+      } else {
+        return Alert.alert("Nenhum repositório encontrado!");
+        setRepo([]);
+      }
+    }
+    busca();
+  }, [estaNaTela]);
+
+  console.log("repo", repo)
+
+  return (
+    <View style={estilos.container}>
+      <Text style={estilos.repositoriosTexto}>
+        {repo.length} repositórios criados
+      </Text>
+      <TouchableOpacity
+        style={estilos.botao}
+        onPress={() =>
+          navigation.navigate("CriarRepositorio", { id: route.params.id })
         }
-        busca()
-    }, [])
-
-
-    return (
-        <View style={estilos.container}>
-                <Text style={estilos.repositoriosTexto}>{repo.length} repositórios criados</Text>
-             <FlatList
-                data={repo}
-                style={{width: '100%'}}
-                keyExtractor={repo => repo.id}      
-                renderItem={({item})=>(
-                    <TouchableOpacity 
-                        style={estilos.repositorio}
-                        onPress={()=> navigation.navigate('InfoRepositorio', {item})} 
-                    >
-                        <Text style={estilos.repositorioNome}>{item.name}</Text>
-                        <Text style={estilos.repositorioData}>Atualizado em {item.data}</Text>
-                    </TouchableOpacity>
-                )}      
-             
-             
-             
-             />
-                <TouchableOpacity 
-                    style={estilos.botao}
-                    onPress={() => navigation.navigate('CriarRepositorio')}
-                >
-                    <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
-                </TouchableOpacity>
-        </View>
-    );
+      >
+        <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={repo}
+        style={{ width: "100%" }}
+        keyExtractor={(repo, index) => repo.id + index}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={estilos.repositorio}
+            onPress={() => navigation.navigate("InfoRepositorio", { item })}
+          >
+            <Text style={estilos.repositorioNome}>{item.name}</Text>
+            <Text style={estilos.repositorioData}>
+              Atualizado em {item.data}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 }
--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -9,3 +9,42 @@ export async function pegarRepositorios (id){
     return []
   }
 }
+
+export async function salvarRepositoriosDoUsuario (postId, nome, data, id){
+  try {
+    const resultado = await api.put(`/repos/${id}`, {
+      name: nome,
+      data: data,
+      postId: postId,
+      id: id
+    });
+    return 'sucesso';
+  } catch (error) {
+    console.log(error);
+    return 'erro'
+  }
+}
+
+export async function criarRepositoriosDoUsuario ({nome,data, postId}){
+  try {
+    const resultado = await api.post(`/repos`, {
+      name: nome,
+      data: data,
+      postId: postId,
+    });
+    return 'Repositório Criado';
+  } catch (error) {
+    console.log(error);
+    return 'Erro ao criar repositório'
+  }
+}
+
+export async function deletarRepositorios(id){
+  try {
+    await api.delete(`/repos/${id}`);
+    return 'sucesso';
+  } catch (error) {
+    console.log(error);
+    return 'erro'
+  }
+}
